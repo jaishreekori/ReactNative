98c39bf4fc2353997fb89a2dc5d5bc8297220fc8
jaishreekori <51723304+jaishreekori@users.noreply.github.com>
2019-06-14 23:02:17 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,97 +1,98 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, ActivityIndicator, FlatList, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Button, TextInput, Text, Switch } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-// type Props = {};
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-      dataSource:[]
-     };
+   constructor() {
+      super();
+      this.state = {
+         switch1Value: false,
+         switch2Value: false,
+         switch3Value: false,
+         email: "",
+      }
+   };
+   handleSubmit = (evt) => {
+      if (!this.validateEmail(this.state.email)) {
+         alert('error !')
+       } else {
+         alert('Data Saved Successfully !')
+       }
+      }
+      
+   toggleSwitch1 = (value) => {
+      this.setState({ switch1Value: value })
+      console.log('Switch 1 is: ' + value)
    }
-  componentDidMount() {
-    fetch("http://dummy.restapiexample.com/api/v1/employees")
-      .then(response => response.json())
-      .then((responseJson) => {
-        console.log(responseJson);
-        this.setState({
-          loading: false,
-          dataSource: responseJson
-        })
-      })
-      .catch(error => console.log(error)) //to catch the errors if any
-  }
-
-  FlatListItemSeparator = () => {
-    return (
-      <View style={{
-        height: .5,
-        width: "100%",
-        backgroundColor: "rgba(0,0,0,0.5)",
-      }}
-      />
-    );
-  }
-  renderItem = (data) =>
-    <TouchableOpacity style={styles.list}>
-      <Text style={styles.lightText}>{data.item.employee_name}</Text>
-      <Text style={styles.lightText}>{data.item.employee_salary}</Text>
-      <Text style={styles.lightText}>{data.item.employee_age}</Text></TouchableOpacity>
-
-
-  render() {
-    if (this.state.loading) {
+   toggleSwitch2 = (value) => {
+      this.setState({ switch2Value: value })
+      console.log('Switch 2 is: ' + value)
+   }
+   toggleSwitch3 = (value) => {
+      this.setState({ switch3Value: value })
+      console.log('Switch 3 is: ' + value)
+   }
+   validateEmail = (email) => {
+      var re = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/'
+      return re.test(email);
+    };
+    
+   render() {
       return (
-        <View style={styles.loader}>
-          <ActivityIndicator size="large" color="#0c9" />
-        </View>
-      )
-    }
-    return (
-      <View style={styles.container}>
-        <FlatList
-          data={this.state.dataSource}
-          ItemSeparatorComponent={this.FlatListItemSeparator}
-          renderItem={item => this.renderItem(item)}
-          keyExtractor={item => item.id.toString()}
-        />
-      </View>
-    )
-  }
+         <View style={styles.container}>
+            <Text style={styles.h2text}>User Details</Text>
+            <Text style={{ justifyContent: 'center', fontSize: 20, marginRight: 20, fontWeight: 'bold' }}>First Name</Text>
+            <TextInput style={styles.input} placeholder='Enter F.Name'>
+            </TextInput>
+            <Text style={{ justifyContent: 'center', fontSize: 20, marginRight: 20, fontWeight: 'bold' }}>Last Name</Text>
+            <TextInput style={styles.input} placeholder='Enter L.Name'></TextInput>
+            <Text style={{ justifyContent: 'center', fontSize: 20, marginRight: 20, fontWeight: 'bold' }}>Email</Text>
+            <TextInput style={styles.input} placeholder='Email'
+               onChange={this.validateEmail}
+            ></TextInput>
+            <Text style={{ justifyContent: 'center', fontSize: 20, marginRight: 20, marginBottom: 10, fontWeight: 'bold' }}>Alerts For :</Text>
+            <Text style={{ justifyContent: 'center', fontSize: 20, marginRight: 20 }}>SMS</Text>
+            <Switch
+               onValueChange={this.toggleSwitch1}
+               value={this.state.switch1Value}
+            />
+            <Text style={{ justifyContent: 'center', fontSize: 20, marginRight: 20 }}>Email</Text>
+            <Switch
+               onValueChange={this.toggleSwitch2}
+               value={this.state.switch2Value}
+            />
+            <Text style={{ justifyContent: 'center', fontSize: 20, marginRight: 20 }}>WhatsApp</Text>
+            <Switch style={{ marginBottom: 20 }}
+               onValueChange={this.toggleSwitch3}
+               value={this.state.switch3Value}
+            />
+            <Button
+               title="Save Details"
+               onPress={this.handleSubmit}
+            />
+         </View>
+      );
+   }
 }
-
-
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff"
-  },
-  loader: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff"
-  },
-  list: {
-    paddingVertical: 4,
-    margin: 5,
-    backgroundColor: "#fff"
-  }
+   container: {
+      justifyContent: 'center',
+      marginTop: 50,
+      padding: 10,
+      backgroundColor: '#ffffff',
+   },
+   input: {
+      margin: 15,
+      height: 40,
+      borderColor: '#7a42f4',
+      borderWidth: 2
+   },
+   h2text: {
+      fontFamily: 'Helvetica',
+      justifyContent: 'center',
+      alignItems: 'center',
+      alignSelf: 'center',
+      fontSize: 30,
+      fontWeight: 'bold',
+   },
 });
+
